7f0bd6a8e66635c501173266001dcb0ad616ed87
Matthieu Drula <matthieu.drula.hs@gmail.com>
2018-04-02 14:26:40 +0200

    all working

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,11 +1,103 @@
-import Vue from 'vue';
-import VueParentNode from './vue-components/parent-node/parent-node.vue';
-import TsParentNode from './typescript-components/parent-node/parent-node';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-var app = new Vue({
-  el: '#app',
-  components: {
-    TsParentNode,
-    VueParentNode
+Vue.use(VueRouter);
+
+const About = {
+  template: "<div>About view</div>",
+  mounted: function() {
+    console.log("Welcome to the About view");
+    const userId = this.$route.params.id;
+
+    if (userId) {
+      console.log("You are viewing the user:", userId);
+    }
+  },
+  destroyed: function() {
+    console.log("Thanks for visiting to the About view");
   }
-})
+};
+
+const Contact = {
+  props: ["id"],
+  template: "<div>Contact view</div>",
+  mounted: function() {
+    console.log("Welcome to the Contact view");
+    const userId = this.id;
+
+    if (userId) {
+      console.log("You are viewing the user", userId);
+    }
+  },
+  destroyed: function() {
+    console.log("Thanks for visiting to the Contact view");
+  }
+};
+
+const NamedRoute = {
+  template: "<div>Named route content</div>"
+};
+
+const FirstChild = {
+  template: "<div>Hi I'm the first child</div>"
+};
+
+const SecondChild = {
+  template: "<div>Hi I'm the second child</div>"
+};
+
+const ParentComponent = {
+  template: `<div>
+      Hi I'm the parent, I will stay here
+      <router-view></router-view>
+      <router-link to="/parent/first-child">First Child</router-link>
+      <router-link to="/parent/second-child">Second Child</router-link>
+    </div>
+  `
+};
+
+const ParentRoute = {
+  path: "/parent",
+  name: "parent",
+  component: ParentComponent,
+  children: [
+    {
+      path: "first-child",
+      component: FirstChild
+    },
+    {
+      path: "second-child",
+      component: SecondChild
+    }
+  ]
+};
+
+const routes = [
+  {
+    path: "/contact/:id",
+    component: Contact,
+    props: true
+  },
+  {
+    path: "/contact",
+    component: Contact
+  },
+  {
+    path: "/about/:id",
+    component: About
+  },
+  {
+    path: "/about",
+    component: About
+  },
+  {
+    path: "/black",
+    name: "white",
+    component: NamedRoute
+  },
+  ParentRoute
+];
+
+const router = new VueRouter({ routes });
+
+const app = new Vue({ router }).$mount("#app");
